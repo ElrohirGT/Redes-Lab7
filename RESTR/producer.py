b168d5e99de1325cac3af7f0a2bce406155b4f7b
ElrohirGT <elrohirgt@gmail.com>
2025-11-13 13:39:34 -0600

    feat: Basic producer done

--- RESTR/producer.py
+++ RESTR/producer.py
@@ -1,53 +1,67 @@
 #!/usr/bin/env python3
 
-from confluent_kafka import Consumer
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+import random
+import math
+import os
 import json
 
+#  N
+# E O
+#  S
+DIRECTIONS = [
+    "N",
+    "NO",
+    "O",
+    "SO",
+    "O",
+    "SE",
+    "E",
+    "NE",
+]
+
 if __name__ == "__main__":
+    load_dotenv()
+
     config = {
         # User-specific properties that you must set
-        "bootstrap.servers": "47.182.219.133",
+        "bootstrap.servers": os.getenv("KAFKA_IP"),
         # Fixed properties
         "group.id": "kafka-python-getting-started",
         "auto.offset.reset": "earliest",
     }
 
     # Create Consumer instance
-    consumer = Consumer(config)
+    producer = Producer(config)
+    topic = os.getenv("TOPIC")
 
-    # Subscribe to topic
-    topic = "22386"
-    consumer.subscribe([topic])
+    timeout = int(os.getenv("TIMEOUT"))
 
     # Poll for new messages from Kafka and print them.
+    def acked(err, msg):
+        if err is not None:
+            print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+        else:
+            print("Message produced: %s" % (str(msg)))
+
     try:
         while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                # Initial message consumption may take up to
-                # `session.timeout.ms` for the consumer group to
-                # rebalance and start consuming
-                print("Waiting...")
-            elif msg.error():
-                print("ERROR: {}".format(msg.error()))
-            else:
-                # Extract the (optional) key and value, and print.
-                msg_key = msg.key().decode("utf-8")
-                msg_value = msg.value().decode("utf-8")
-                print(
-                    "Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
-                        topic=msg.topic(), key=msg_key, value=msg_value
-                    )
-                )
-
-                jsonObj = json.loads(msg_value)
-
-                temperature = jsonObj["temperatura"]
-                humidity = jsonObj["humedad"]
-                wind = jsonObj["direccion_viento"]
+            obj = {
+                "temperatura": random.gauss(25, 10),
+                "humedad": math.ceil(random.gauss(25, 10)),
+                "direccion_viento": random.choice(DIRECTIONS),
+            }
+            jsonStr = json.dumps(obj)
+            producer.produce(topic, key="sensor1", value=jsonStr, callback=acked)
+            # Wait up to 1 second for events. Callbacks will be invoked during
+            # this method call if the message is acknowledged.
+            producer.poll(timeout)
 
     except KeyboardInterrupt:
         pass
+    except Exception as e:
+        print("FATAL ERROR: {}".format(e))
     finally:
         # Leave group and commit final offsets
-        consumer.close()
+        producer.close()
